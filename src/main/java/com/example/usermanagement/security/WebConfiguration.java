79afb52f23378f02859235957e7677137067076a
Mingeun Park <mindal99546@gmail.com>
2023-04-17 20:35:34 +0900

    config(CORS): 임의의 origin에 대한 CORS 허용

---
+++ src/main/java/com/example/usermanagement/security/WebConfiguration.java
@@ -0,0 +1,16 @@
+package com.example.usermanagement.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+    //TODO 프론트 엔드 애플리케이션만 허용하도록 변경
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "DELETE", "PATCH");
+    }
+}
